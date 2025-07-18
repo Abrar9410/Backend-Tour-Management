a6711054420ebab72939ed0d7246690460d14ac5
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-07-18 21:25:53 +0600

    Local Passport Login implemented

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,10 +1,53 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
 import { Strategy as GoogleStrategy, Profile, VerifyCallback } from "passport-google-oauth20";
+import bcryptjs from "bcryptjs";
 import { envVars } from "./env";
 import { Users } from "../modules/user/user.model";
 import { Role } from "../modules/user/user.interface";
 
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: "email",
+            passwordField: "password"
+        },
+        async (email: string, password: string, done) => {
+            try {
+                const user = await Users.findOne({ email });
+
+                if (!user) {
+                    return done(null, false, {message: "User Does Not Exist!"});
+                    // return done("User Does Not Exist!")  //Alternative
+                };
+
+                const isGoogleAuthenticated = user.auths.some(providerObjects => providerObjects.provider === "google");
+                if (isGoogleAuthenticated && !user.password) {
+                    return done(
+                        null,
+                        false,
+                        { message: "You have authenticated with Google previously! If you want to enable Credentials login, first login with Google with this gmail and set a password for your account." }
+                    );
+                    // Alternative
+                    // return done("You have authenticated with Google previously! If you want to enable Credentials login, first login with Google with this gmail and set a password for your account.")
+                };
+
+                const isPasswordMatched = await bcryptjs.compare(password as string, user.password as string);
+
+                if (!isPasswordMatched) {
+                    return done(null, false, { message: "Incorrect Password!" });
+                };
+
+                return done(null, user);
+            } catch (error) {
+                console.log(error);
+                done(error);
+            }
+        }
+    )
+)
 
 passport.use(
     new GoogleStrategy(
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,48 +1,48 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Users } from "../user/user.model";
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
+// import { IUser } from "../user/user.interface";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
-import { createNewTokenWithRefreshToken, createUserTokens } from "../../utils/userTokens";
+import { createNewTokenWithRefreshToken, /*createUserTokens*/ } from "../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
 
 
-const credentialsLoginService = async (payload: Partial<IUser>) => {
-    const {email, password} = payload;
+// const credentialsLoginService = async (payload: Partial<IUser>) => {
+//     const {email, password} = payload;
 
-    const user = await Users.findOne({ email });
+//     const user = await Users.findOne({ email });
 
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "User Does Not Exist!");
-    };
+//     if (!user) {
+//         throw new AppError(httpStatus.NOT_FOUND, "User Does Not Exist!");
+//     };
 
-    const isGoogleAuthenticated = user.auths.some(providerObjects => providerObjects.provider === "google");
-    if (isGoogleAuthenticated) {
-        throw new AppError(
-            httpStatus.CONFLICT,
-            "You have authenticated with Google previously! If you want to enable Credentials login, first login with Google with this gmail and set a password for your account."
-        );
-    };
+//     const isGoogleAuthenticated = user.auths.some(providerObjects => providerObjects.provider === "google");
+//     if (isGoogleAuthenticated && !user.password) {
+//         throw new AppError(
+//             httpStatus.CONFLICT,
+//             "You have authenticated with Google previously! If you want to enable Credentials login, first login with Google with this gmail and set a password for your account."
+//         );
+//     };
 
-    const isPasswordMatched = await bcryptjs.compare(password as string, user.password as string);
+//     const isPasswordMatched = await bcryptjs.compare(password as string, user.password as string);
 
-    if (!isPasswordMatched) {
-        throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password!")
-    };
+//     if (!isPasswordMatched) {
+//         throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password!")
+//     };
 
-    const { token, refreshToken } = createUserTokens(user);
+//     const { token, refreshToken } = createUserTokens(user);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {password: pass,...rest} = user.toObject();
+//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//     const {password: pass,...rest} = user.toObject();
 
-    return {
-        token,
-        refreshToken,
-        user: rest
-    };
-};
+//     return {
+//         token,
+//         refreshToken,
+//         user: rest
+//     };
+// };
 
 const getNewTokenService = async (refreshToken: string) => {
     
@@ -86,7 +86,7 @@ const resetPasswordService = async (decodedToken: JwtPayload, oldPassword: strin
 };
 
 export const AuthServices = {
-    credentialsLoginService,
+    // credentialsLoginService,
     getNewTokenService,
     resetPasswordService
 };
